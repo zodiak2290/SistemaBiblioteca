d8f52ee60ec0dfe049530b1de7aacc141fedf730
Alberto <albon_marvel@hotmail.com>
2016-07-10 23:29:04 -0500

    Cambiando texto biblioteca

--- application/views/inicio/conocenos.php
+++ application/views/inicio/conocenos.php
@@ -3,21 +3,16 @@
     <div class="box">
 		<div class="col-lg-12 text-center">
 		        <span class="columna1_texto">
-		         <p><strong>BIBLIOTECA PÚBLICA DE OAXACA </strong></p>
+		         <p><strong>Titulo </strong></p>
 		         <p align="justify"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
 		        <p align="justify"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
-		         <p><strong>BIBLIOTECA  </strong></p>
-		         <p align="justify"> La Biblioteca Pública Central de Oaxaca Margarita Maza de Juárez se encuentra a dos cuadras del zócalo de la ciudad
-					sobre la calle Macedonio Alcalá, en esquina con calle Morelos. Sus instalaciones ocupan una casa de principios del XVIII,
-					representativa de la arquitectura civil de la ciudad. Fue remodelada en el periodo contemporáneo por los arquitectos 
-					Enrique de Esesarte y Jorge Torres. Inaugurada formalmente el 29 de octubre de 1985, como un organismo dependiente del sector público, 
-					actualmente forma parte de la Dirección General de Bibliotecas y está adscrita a la Secretaría de las Culturas y Artes de Oaxaca.</p>
-		        <p align="justify"> Encabeza como biblioteca central, a una red estatal de bibliotecas a cargo de la Coordinación Estatal de Bibliotecas.
-				Existe una trama de bibliotecas de la ciudad, del sector público y de la sociedad civil, denominada Círculo de Bibliotecas de Oaxaca, de la que también forma parte.
-				La Biblioteca Pública tiene, además de su acervo general, un acervo valioso de libros históricos sobre temas y autores oaxaqueños, conformada por varias colecciones de diverso origen.
-				Entre estos materiales resguarda memorias manuscritas del General Ignacio Mejía, periódicos, folletos, boletines y revistas oaxaqueñas de época como La Crónica (1907), Oaxaca Moderno (1923) y el Boletín de la Biblioteca Pública del Estado (1920).
-				En sus instalaciones se llevan a cabo presentaciones de libros, lectura de obra poética y literaria, exposiciones, encuentros, foros, charlas, conferencias y diversas actividades culturales.
-				Sus instalaciones son visitadas por estudiantes, investigadores y público en general, pues es de acceso gratuito.</p>
+		         <p><strong>Subtema  </strong></p>
+		         <p align="justify"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
+
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+		        <p align="justify"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..
+
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
 				<p><strong> NUESTRA MISIÓN </strong></p> 
 				<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		        
